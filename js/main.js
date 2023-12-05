b905dbbcc771bd029fca0f74d897a05dfc11eb4a
kenjayev <kenjayevazizbek2oo4@gmail.com>
2023-12-05 19:16:02 +0300

    add other coder type

--- js/main.js
+++ js/main.js
@@ -3,11 +3,24 @@ let inputValue = document.getElementById("inputValue"),
     outputValue = document.getElementById("outputValue"),
     inputValueLabel = document.getElementById("inputValueLabel"),
     copyBtn = document.getElementById("copy-btn"),
-    modal = document.getElementById("modal");
-(keyKod = 15), (isYourWords = true);
+    modal = document.getElementById("modal"),
+    typeCodeSelect = document.getElementById("typeCode"),
+    keyKod = -3,
+    isYourWords = true;
 
+typeCodeSelect.addEventListener("change", (e) => {
+    if (typeCodeSelect.value == "sezer") {
+        keyKod = -3;
+    } else if (typeCodeSelect.value == "sezer_pro_max") {
+        keyKod = 15;
+    }
+
+    mainStartCode();
+});
 // Each changes use this function and output result
-inputValue.addEventListener("input", () => {
+inputValue.addEventListener("input", mainStartCode);
+
+function mainStartCode() {
     // ASCII numbers arr, coding numbers
     let stringCharsCodes = inputValue.value.split("").map((item) => {
         let result = isYourWords ? shifirla(item) : qaytar(item);
@@ -22,42 +35,7 @@ inputValue.addEventListener("input", () => {
 
     //output our result
     outputValue.value = newString;
-    if (outputValue.value.length === 0) {
-        copyBtn.style.display = "none";
-    } else if (outputValue.value.length > 0) {
-        copyBtn.style.display = "block";
-    }
-});
-
-// replays result function, it call in html
-function almashtir() {
-    isYourWords = !isYourWords;
-
-    // replay values
-    let inputValueV = inputValue.value;
-    inputValue.value = outputValue.value;
-    outputValue.value = inputValueV;
-
-    // Auto focus input Area
-    inputValue.focus();
-
-    // input change for decode
-    if (!isYourWords) {
-        inputValueLabel.textContent = "Shifirlangan matnni kriting: ";
-        inputValue.setAttribute(
-            "placeholder",
-            "Shifirlangan Kodni bu yerga joylang"
-        );
-    }
-
-    // input change for code
-    else {
-        inputValueLabel.textContent = "Shifirlanuvchi so'z: ";
-        inputValue.setAttribute(
-            "placeholder",
-            "Shifirlanuvchi so'zni bu yerga kiriting"
-        );
-    }
+    privetCopy(outputValue);
 }
 
 // Coding ASCII numbers
@@ -92,6 +70,7 @@ function qaytar(item) {
     return result;
 }
 
+// Copy your result value
 function copyResult() {
     console.log(outputValue.value);
     navigator.clipboard.writeText(outputValue.value);
@@ -99,4 +78,44 @@ function copyResult() {
     setTimeout(() => {
         modal.style.display = "none";
     }, 2500);
+}
+
+// hidden or block Copy img element
+function privetCopy(outputValue) {
+    if (outputValue.value.length === 0) {
+        copyBtn.style.display = "none";
+    } else if (outputValue.value.length > 0) {
+        copyBtn.style.display = "block";
+    }
+}
+
+// replays result function, it call in html
+function almashtir() {
+    isYourWords = !isYourWords;
+
+    // replay values
+    let inputValueV = inputValue.value;
+    inputValue.value = outputValue.value;
+    outputValue.value = inputValueV;
+
+    // Auto focus input Area
+    inputValue.focus();
+
+    // input change for decode
+    if (!isYourWords) {
+        inputValueLabel.textContent = "Shifirlangan matnni kriting: ";
+        inputValue.setAttribute(
+            "placeholder",
+            "Shifirlangan Kodni bu yerga joylang"
+        );
+    }
+
+    // input change for code
+    else {
+        inputValueLabel.textContent = "Shifirlanuvchi so'z: ";
+        inputValue.setAttribute(
+            "placeholder",
+            "Shifirlanuvchi so'zni bu yerga kiriting"
+        );
+    }
 }
